64a9ab8a862d3b0c5c5c0a486db37443831a862c
kerem <keremelma388@outlook.com>
2025-10-09 00:04:52 +0300

    kvkk metni eklendi, checkbox'u eklendi, çalışma saatleri düzenlendi

--- clinique/clinique/urls.py
+++ clinique/clinique/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('iletisim/', TemplateView.as_view(template_name='pages/iletisim.html'), name='iletisim'),
     path('contact/submit/', send_message, name='send-message'),
     path('hakkimda/', hakkimda, name='hakkimda'),
-    path('kvkk/', TemplateView.as_view(template_name='pages/kvkk.html'), name='kvkk'),
 ]
